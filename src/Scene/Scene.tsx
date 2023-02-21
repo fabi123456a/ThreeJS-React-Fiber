e6a2266b0cdaf4d4184ae04b50b275d4b2ba8017
fabi123456a <fabi.auderath@googlemail.com>
2023-02-21 23:06:11 +0100

    add wireframe ground

--- src/Scene/Scene.tsx
+++ src/Scene/Scene.tsx
@@ -2,6 +2,8 @@ import SceneModel from "../3D-Objects/SceneModel";
 import Room from "../3D-Objects/Room";
 import { Camera } from "./Camera";
 import { useThree } from "@react-three/fiber";
+import { useEffect } from "react";
+import * as THREE from "three";
 
 //const deg2rad = (degrees: number) => degrees * (Math.PI / 180);
 
@@ -19,6 +21,24 @@ export default function Scene(props: {
   const { scene } = useThree();
   props.sceneRef.current = scene;
 
+  useEffect(() => {
+    for (let j: number = -500; j < 500; j += 50) {
+      for (let i: number = -500; i < 500; i += 50) {
+        const geometry = new THREE.BoxGeometry(50, 0, 50);
+        const material = new THREE.MeshBasicMaterial({
+          color: 0xf5f5f5,
+          wireframe: true,
+          opacity: 0.5,
+        });
+        const mesh = new THREE.Mesh(geometry, material);
+        mesh.position.set(i, 0, j);
+        scene.add(mesh);
+      }
+    }
+
+    //alert("s");
+  }, []);
+
   return (
     <>
       {/* Canvas nimmt größe von parent container */}
@@ -30,7 +50,7 @@ export default function Scene(props: {
         perspektive={props.perspektive}
       ></Camera>
       {/* Licht */}
-      <ambientLight intensity={0.1}/>
+      <ambientLight intensity={0.1} />
       <pointLight position={[10, 10, 10]} />
       {/* Modelle */}
       {props.models.map((model) => (
