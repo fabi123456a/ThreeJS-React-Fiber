d2415ef5b54e4ce171b8a537317be306224b88d3
godslayer19 <monteirom1337@gmail.com>
2023-02-22 17:31:24 +0100

    update(not complet)

--- src/utils/exporting.ts
+++ src/utils/exporting.ts
@@ -14,26 +14,35 @@ export default function exportToGLTF(scene: Object3D) {
         saveArrayBuffer(result, "scene.glb");
       } else {
         const output = JSON.stringify(result, null, 2);
-        saveString(output, "scene.gltf");
+        savedirect(output, "scene.gltf");
       }
     },
     options
   );
 }
 
-const link = document.createElement("a");
-document.body.appendChild(link);
 
-function save(blob: Blob, filename: string) {
-  link.href = URL.createObjectURL(blob);
+
+function savedirect(out: string, filename: string) {
+  const link = document.createElement("a");
+  document.body.appendChild(link);
+  link.href = URL.createObjectURL(
+    new Blob([out], { type: "model/gltf-binary" })
+  );
   link.download = filename;
   link.click();
 }
 
-function saveString(text: string, filename: string) {
+/*function saveString(text: string, filename: string) {
   save(new Blob([text], { type: "text/plain" }), filename);
+}*/
+function save(blob: Blob, filename: string) {
+  const link = document.createElement("a");
+  document.body.appendChild(link);
+  link.href = URL.createObjectURL(blob);
+  link.download = filename;
+  link.click();
 }
-
 function saveArrayBuffer(buffer: any, filename: string) {
   save(new Blob([buffer], { type: "application/octet-stream" }), filename);
 }
